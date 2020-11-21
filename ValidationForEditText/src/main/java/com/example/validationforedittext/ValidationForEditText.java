f024a9d3470216cfcdff504cc85a3841bb3de5ed
turjemantal <turjemantal20@gmail.com>
2020-11-21 18:43:30 +0200

    first changes

--- ValidationForEditText/src/main/java/com/example/validationforedittext/ValidationForEditText.java
+++ ValidationForEditText/src/main/java/com/example/validationforedittext/ValidationForEditText.java
@@ -1,8 +1,15 @@
 package com.example.validationforedittext;
 
 public class ValidationForEditText {
+
+
     static public boolean emailIsValid(String email) {
         String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+        return email.matches(regex);
+    }
+    static public boolean israeliPhoneNumbers(String email) {
+        String regex = "^0(?:[234689]|5[0-689]|7[246789])(?![01])(\\d{7})$\n";
+
         return email.matches(regex);
     }
     static public boolean isOnlyCharacters(String name) {
